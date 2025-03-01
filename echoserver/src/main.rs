4a478f0b7b968e9c8a953377ca74994446bf07b4
niikun <niikun0209@gmail.com>
2025-03-01 12:03:32 +0900

    echoserver

--- echoserver/src/main.rs
+++ echoserver/src/main.rs
@@ -11,18 +11,18 @@ fn main()->Result<(),Box<dyn Error>>{
 }
 
 fn echo_server(addr:&str)->Result<(),Box<dyn Error>>{
-    let listener = TcpListener::bind(addr)?;
+    let listener = TcpListener::bind(addr)?; //1 bind to the address
     loop{
-        let (mut stream,_) = listener.accept()?;
-        thread::spawn(move ||{
+        let (mut stream,_) = listener.accept()?; //2 accept the connection
+        thread::spawn(move ||{  //3 spawn a thread to handle the connection
             let mut buffer = [0u8;1024];
             loop{
-                let nbytes = stream.read(&mut buffer).unwrap();
-                if nbytes == 0{
+                let nbytes = stream.read(&mut buffer).unwrap(); //4 read from the stream
+                if nbytes == 0{  //6 if no bytes are read, the connection is closed
                     return;
                 }
                 print!("{}",str::from_utf8(&buffer[..nbytes]).unwrap());
-                stream.write_all(&buffer[..nbytes]).unwrap();
+                stream.write_all(&buffer[..nbytes]).unwrap(); //5 write to the stream
             }
         });
     }
